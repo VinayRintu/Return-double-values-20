db7343021fbdac75d18a4dec9a212bcf27c66980
Rintu <Rintu@VINDUJA>
2022-10-31 16:18:34 +0530

    Add project files.

---
+++ Return double values 20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_double_values_20
+{
+    internal class Program
+    {
+        void dife()
+        {
+            Console.WriteLine("enter first number");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter first number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                int x=(a - b) * 2;
+                Console.WriteLine(x);
+            }
+            else
+            {
+                int y=b-a;
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.dife();
+            
+        }
+    }
+}
